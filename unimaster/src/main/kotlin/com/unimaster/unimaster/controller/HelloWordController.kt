afc37b631fdba200830f4b3926f98e8f6a296c06
Pedro Bruno Fernandes <pedro.nascimento@maximatech.com.br>
2023-11-08 19:53:06 -0300

    Cors
    Alterado cors dos métodos de get

--- unimaster/src/main/kotlin/com/unimaster/unimaster/controller/HelloWordController.kt
+++ unimaster/src/main/kotlin/com/unimaster/unimaster/controller/HelloWordController.kt
@@ -3,12 +3,14 @@ package com.unimaster.unimaster.controller
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.springframework.web.bind.annotation.CrossOrigin
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/hello")
+@CrossOrigin(origins = ["*"], allowedHeaders = ["*"])
 class HelloWordController {
 
     @GetMapping
